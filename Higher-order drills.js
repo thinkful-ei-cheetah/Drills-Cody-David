1420d7b01db113aa35a08ac2708ff27ceb62f9ff
Cody Gillette <cgillette1312@gmail.com>
2019-03-26 13:00:40 -0700

    filter

--- Higher-order drills.js
+++ Higher-order drills.js
@@ -19,3 +19,33 @@ function goodbye() {
 // repeat(goodbye,5);
 
 
+function filter(arr,fn) {
+    let newArray = []
+    arr.map(word => {
+        if(fn(word) === true){
+            newArray.push(word)
+        }
+    })
+    return newArray;
+}
+//challange 
+// function filter(arr,fn) {
+//     let newArray = []
+//     arr.map(word => fn(word) && word[0] ? newArray.push(word): undefined )
+//     return newArray
+       
+// }
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+    // This is known as a "predicate function" - it's a function that 
+    // only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
