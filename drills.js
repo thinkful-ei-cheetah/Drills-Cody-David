5ce855eeee0e250dae9f80f16c384cda09207fe2
David Nordeen <davidnordeen@gmail.com>
2019-03-26 11:18:05 -0700

    max min finished

---
+++ drills.js
@@ -0,0 +1,25 @@
+//1. curent max & min
+
+function max(numbers) {
+    let currentMax = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        if (numbers[i] > currentMax) {
+            currentMax = numbers[i];
+        }
+        i++;
+    }
+    return currentMax;
+}
+
+function min(numbers) {
+    let currentMin = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        if (numbers[i] < currentMin) {
+            currentMin = numbers[i];
+        }
+        i++;
+    }
+    return currentMin;
+}
